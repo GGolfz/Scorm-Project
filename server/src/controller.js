f98b326eb70efa1fcb92290f68f7277898c5b266
GGolfz <wisarutgolf11@gmail.com>
2021-06-30 15:12:37 +0700

    add: get course & update progress :sparkles:

--- server/src/controller.js
+++ server/src/controller.js
@@ -4,6 +4,7 @@ const multer = require("multer");
 const fs = require("fs");
 const uzip = require("unzipper");
 const path = require("path");
+const pool = require("./db");
 
 const fileStorage = multer.diskStorage({
   destination: "src/uploads/",
@@ -11,7 +12,7 @@ const fileStorage = multer.diskStorage({
 });
 
 router.get("/test", async (req, res) => {
-  res.status(200).json({ status: "success" });
+  return res.status(200).json({ status: "success" });
 });
 
 const upload = multer({ storage: fileStorage });
@@ -23,7 +24,21 @@ router.post("/upload", upload.single("file"), async (req, res) => {
   let zipPath = path.join("src/uploads/"+file.originalname)
   await fs.createReadStream(zipPath).pipe(uzip.Extract({path:filePath}))
   await fs.unlinkSync(zipPath)
-  res.status(201).json({success:true})
-
+  await pool.query("INSERT INTO courses(name) VALUES ($1)",[name.join(".")])
+  return res.status(201).json({success:true})
 });
+router.get("/course", async (req,res) => {
+  const courses = await pool.query("SELECT * FROM courses");
+  return res.send(courses.rows)
+})
+router.post("/progess", async (req,res) => {
+  const {courseId,userId,location,status,score} = req.body.courseId;
+  let exist = await pool.query("SELECT * FROM progress WHERE course_id = $1 AND user_id = $2",[courseId,userId])
+  if(exist.rowCount > 0){
+    await pool.query("UPDATE progress SET location = $1,status = $2, score = $3 WHERE course_id = $4 AND user_id = $5",[location,status,score,courseId,userId])
+  } else {
+    await pool.query("INSERT INTO progress(course_id,user_id,location,status,score)",[courseId,userId,location,status,score])
+  }
+  return res.send({success:true})
+})
 module.exports = router;
